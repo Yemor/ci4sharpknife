b8b9682c69bbaf543e00a8884067e1630fd03ffd
yemor <albert971@qq.com>
2020-04-04 10:33:23 +0800

    change url to .env and add gitclone_bash

--- ci/app/Config/App.php
+++ ci/app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://www.yemor.cn:80/';
+	public $baseURL = '';
 
 	/*
 	|--------------------------------------------------------------------------
