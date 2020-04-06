fe71bc9dfe0eaeb6f43d9429d3dbd88abfe4e9a8
yemor <albert971@qq.com>
2020-04-07 02:18:49 +0800

    fix home to Home.

--- ci/app/Controllers/Pages.php
+++ ci/app/Controllers/Pages.php
@@ -4,14 +4,14 @@ namespace App\Controllers;
 
 class Pages extends BaseController
 {
-    public function view($page = 'home')
+    public function view($page = 'Home')
     {
         if (!file_exists(APPPATH.'/Views/Pages/'.$page.'.php')) {
             // Whoops, we don't have a page for that!
             throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
         }
 
-        $data['title'] = ucfirst($page); // Capitalize the first letter
+        $data['title'] = $page; // Capitalize the first letter
         
         echo view('Templates/Header', $data);
         echo view('Templates/Bootstrap', $data);
