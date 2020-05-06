c7a31553374da635ddd6d9b22a681ed70df4f0e8
yemor <albert971@qq.com>
2020-05-06 08:23:04 +0800

    add recent data.

--- ci/app/Controllers/Sharpknife.php
+++ ci/app/Controllers/Sharpknife.php
@@ -2,63 +2,21 @@
 
 namespace App\Controllers;
 
-class Sharpknife extends BaseController
+class Sharpknife extends \CodeIgniter\Controller
 {
-    public $ctl_data = [];
+    
+    use \CodeIgniter\API\ResponseTrait;
 
-	/**
-	 * Constructor.
-	 */
-	public function initController(\CodeIgniter\HTTP\RequestInterface $request, \CodeIgniter\HTTP\ResponseInterface $response, \Psr\Log\LoggerInterface $logger)
-	{
-		// Do Not Edit This Line
-        parent::initController($request, $response, $logger);
-		
-		
-        $this->ctl_data['viewport'] = "idth=device-width, initial-scale=1, shrink-to-fit=no";
-        $this->ctl_data['description'] = "This is a Pages for view";
-        $this->ctl_data['author'] = "Yemor";
-
-        $this->ctl_data['cssList'] = array(
-			//Bootstrap CSS
-			array(
-				'href'  => 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
-				'rel'   => 'stylesheet',
-				'integrity'  => 'sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh',
-				'crossorigin' => 'anonymous'
-			)
-		);
-        $this->ctl_data['jsList'] = array(
-			//jQuery JS
-			array(
-				'src'  => 'https://code.jquery.com/jquery-3.4.1.slim.min.js',
-				'integrity'  => 'sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n',
-				'crossorigin' => 'anonymous'
-			),
-			//POPPER JS
-			array(
-				'src'  => 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
-				'integrity'  => 'sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo',
-				'crossorigin' => 'anonymous'
-			),
-			//BOOTSTRAP JS
-			array(
-				'src'  => 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',
-				'integrity'  => 'sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6',
-				'crossorigin' => 'anonymous'
-			),
-        );
-        $this->ctl_data['refreshtime'] = '';
-        // echo "<p>initController</p>" ;
-        // echo json_encode($this->ctl_data);
-	}
     public function view($page = 'networkData')
     {
+        $this->response->setHeader('Access-Control-Allow-Origin', '*');
+        $this->response->setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        $this->response->setContentType('application/json');
         $db = \Config\Database::connect();
-        $query = $db->query('SELECT * FROM net_sharpknife.network_flow;');
+        $query = $db->query('SELECT * FROM net_sharpknife.network_flow ORDER BY flow_timestamp DESC LIMIT 10;');
         $results = $query->getResult();
 
-        echo json_encode($results);
+        return json_encode($results);
         // foreach ($results as $row)
         // {
         //     foreach ($row as $val)
--- ui design/origin_moudle/home/home.js
+++ ui design/origin_moudle/home/home.js
@@ -1,39 +1,35 @@
+var recentdata = new Vue({
+    el: '#recentdata',
+    data: {
+        ths: [
+            { text: "数据包编号" },
+            { text: "数据内容" },
+            { text: "交付时间" },
+            { text: "长度" }
+        ],
+        trs: [
+        ]
+    }
+})
 
 var recentip = new Vue({
     el: '#recentip',
     data: {
         ths: [
-            {text: "排名"},
-            {text: "频繁访问IP"},
-            {text: "最后访问时间"},
-            {text: "访问次数"}
+            { text: "排名" },
+            { text: "频繁访问IP" },
+            { text: "最后访问时间" },
+            { text: "访问次数" }
         ],
         trs: [
-            {num:1, tds:['A','B','C']},
-            {num:2, tds:['D','E','F']},
-            {num:3, tds:['G','H','I']},
-            {num:4, tds:['J','K','L']}
+            { num: 1, tds: ['A', 'B', 'C'] },
+            { num: 2, tds: ['D', 'E', 'F'] },
+            { num: 3, tds: ['G', 'H', 'I'] },
+            { num: 4, tds: ['J', 'K', 'L'] }
         ]
     }
-    })
+})
 
-var recentdata = new Vue({
-    el: '#recentdata',
-    data: {
-        ths: [
-            {text: "数据包编号"},
-            {text: "包含协议"},
-            {text: "交付时间"},
-            {text: "长度"}
-        ],
-        trs: [
-            {num:1, tds:['A','B','C']},
-            {num:2, tds:['D','E','F']},
-            {num:3, tds:['G','H','I']},
-            {num:4, tds:['J','K','L']}
-        ]
-    }
-    })
 
 
 var chart = echarts.init(document.getElementById('netflow_echarts'));
@@ -43,8 +39,8 @@ var data1 = [];
 var data2 = [];
 for (var i = 0; i < 100; i++) {
     xAxisData.push('类目' + i);
-    data1.push((Math.sin(i / 5) * (i / 5 -10) + i / 6) * 5);
-    data2.push((Math.cos(i / 5) * (i / 5 -10) + i / 6) * 5);
+    data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+    data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
 }
 
 option = {
@@ -97,3 +93,68 @@ option = {
 };
 
 chart.setOption(option);
+
+$(function()
+{
+    // console.log("test");
+    // var request = new XMLHttpRequest();
+    // request.open("GET", "http://localhost/sharpknife/view/networkdata");
+    // request.setRequestHeader('content-type', 'application/json');
+    // request.withCredentials = true;
+    // request.send();
+    // request.onreadystatechange = function() {
+    //     if(request.readyState==4 && request.status==200)
+    //     {
+    //         console.log(request.response)
+    //         // var res = JSON.parse(request.response);
+    //         // console.log(res);
+    //     }
+    //     else
+    //     {
+    //         alert("aaa");
+    //     }
+    // }
+    console.log(recentdata)
+    // axios.get('http://localhost/sharpknife/view/networkdata')
+    // .then(function (response) {
+    //     console.log(response.data);
+    //     console.log(typeof(JSON.stringify(response.data)));
+    //     // recentdata.trs=response.data;
+    //     // console.log(recentdata.trs);
+    // })
+    // .catch(function (error) {
+    //     console.log(error);
+    // });
+    result = $.ajax({
+        method: "POST",
+        url: "http://localhost/sharpknife/view/networkdata",
+        success: function (data) {
+            console.log("success");
+            console.log(data);
+            console.log(recentdata);
+            recentdata.trs=data;
+        },
+        error: function (data) {
+            console.log("error");
+            alert("初始化请求失败");
+        },
+        complete:function(data) {
+            console.log("complete");
+        }
+    });
+    // console.log("after");
+    // console.log(result.responseText);
+    // console.log(result);
+    // recentdata = new Vue({
+    //     el: '#recentdata',
+    //     data: {
+    //         ths: [
+    //             { text: "数据包编号" },
+    //             { text: "数据内容" },
+    //             { text: "交付时间" },
+    //             { text: "长度" }
+    //         ],
+    //         trs: result
+    //     }
+    // })
+})
